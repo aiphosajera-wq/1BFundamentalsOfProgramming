6687e5a587c09ae5b3384af5e49faf74b86a6b63
aiphosajera-wq <aiphosajera@gmail.com>
2026-08-28 14:26:38 +0800

    hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Hello", name)
+    print("You are an adult.")
+else:
+    print("Hello", name)
+    print("You are a minor.")
