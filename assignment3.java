b3508677a8e0618c5172bd34d06b498ee7bf09c5
aiphosajera-wq~ <aiphosajera@gmail.com>
2026-09-03 04:19:43 +0800

    Add assignment3.java

---
+++ assignment3.java
@@ -0,0 +1,26 @@
+public class assignment3{
+    //This indicates the name of the class
+    //The indicate the name of the class is the two way of creating comment
+
+public static void main(String[]args){
+
+// The main method is the starting point a Java program
+String firstname = "Sophia";
+String lastname = "Ajera";
+String address = "Sta. Barbara";
+String mobilenumber = "09123325781";
+// String represent data types that contains multiple character
+int age = 17;
+//Integer literals default to the data type int.(use in whole number)
+double grade = 96.50;
+// Floating point literals default to the data type dou ble which is a 64-bit value
+
+System.out.println("First Name:" + firstname );
+System.out.println("Last Name:" + lastname );
+System.out.println("Address:" + address);
+System.out.println("Mobile Number:" + mobilenumber );
+System.out.println("Age:" + age);
+System.out.println("Grade:" + grade );
+
+}
+}
